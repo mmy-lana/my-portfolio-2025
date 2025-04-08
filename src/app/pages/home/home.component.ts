7020bf9de13ec7911231ee97bba737771f653329
@m.maulanayusuf777 <mmaulanayusuf777@gmail.com>
2025-04-08 15:14:09 +0700

    add init About

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { AboutComponent } from '../../components/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
