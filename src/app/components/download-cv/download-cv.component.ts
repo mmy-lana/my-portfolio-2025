724cd22e2f77681b0889874da8746db2d886cd28
@m.maulanayusuf777 <mmaulanayusuf777@gmail.com>
2025-04-08 14:01:09 +0700

    initial setup folder structure

---
+++ src/app/components/download-cv/download-cv.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download-cv',
+  imports: [],
+  templateUrl: './download-cv.component.html',
+  styleUrl: './download-cv.component.css'
+})
+export class DownloadCvComponent {
+
+}
