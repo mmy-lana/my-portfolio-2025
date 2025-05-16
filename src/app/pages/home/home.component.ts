cab447e406035d4c3d94060f1100a096feb9198b
@m.maulanayusuf777 <mmaulanayusuf777@gmail.com>
2025-05-16 14:19:02 +0700

    Feat : add scroll to section,

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { AboutComponent } from '../../components/about/about.component';
 import { SkillsComponent } from '../../components/skills/skills.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, AboutComponent, SkillsComponent],
+  imports: [NavbarComponent, AboutComponent, SkillsComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  activeSection = 'about';
+
+  ngAfterViewInit() {
+    const options = {
+      threshold: 0.6 // 60% in view
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const sectionId = entry.target.id;
+          this.activeSection = sectionId;
+        }
+      });
+    }, options);
+
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => observer.observe(section));
+  }
 
 }
